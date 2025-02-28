da1ee114d3175b4436b814c9faabbc60e8afc5c7
seahpark247 <famousjedo@gmail.com>
2025-02-28 12:58:49 -0600

    Initial Commit

---
+++ GuessTheFlag6/GuessTheFlag6App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag6App.swift
+//  GuessTheFlag6
+//
+//  Created by Seah Park on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessTheFlag6Tests/GuessTheFlag6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag6Tests.swift
+//  GuessTheFlag6Tests
+//
+//  Created by Seah Park on 2/28/25.
+//
+
+import Testing
+@testable import GuessTheFlag6
+
+struct GuessTheFlag6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
